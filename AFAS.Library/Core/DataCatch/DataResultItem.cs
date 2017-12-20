e1ed916005bb2050fbd56659c7e63bd6fc4dc269
DarkPally <593517899@qq.com>
2017-12-20 14:32:11 +0800

    修复忽略的

---
+++ AFAS.Library/Core/DataCatch/DataResultItem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AFAS.Library
+{
+    public class DataResultItem
+    {
+       
+       public DataTable Table { get; set; }
+
+       public FileCatchResultItem ParentFile { get; set; }
+       public DataResultItem ParentData { get; set; }
+
+       string desc;
+       public string Desc
+        {
+            get
+            {
+                if(desc==null)
+                {
+                    return MarkInfo.TableDesc;
+                }
+                else
+                {
+                    return desc;
+                }
+            }
+            set
+            {
+                desc = value;
+            }
+        }
+
+       List<DataResultItem> children;
+       public List<DataResultItem> Children
+        {
+            get
+            {
+                return  children ?? (children = new List<DataResultItem>());
+            }
+            set
+            {
+                children = value;
+            }
+        }
+
+       DataMarkInfo markInfo;
+       public DataMarkInfo MarkInfo
+        {
+            get
+            {
+                if (markInfo == null)
+                {
+                    if (ParentData != null) return ParentData.markInfo;
+                }
+                return markInfo;
+            }
+            set
+            {
+                markInfo = value;
+            }
+        }
+    }
+}
